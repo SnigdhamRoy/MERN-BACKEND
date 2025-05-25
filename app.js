424562aa8cd2784000eb48bf2c2547f2b63760a2
Snigdham Roy <roysnigdham@gmail.com>
2025-05-25 21:29:29 +0530

    Changed

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enstlhj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
