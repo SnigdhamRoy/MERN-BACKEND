bedec5495f424d3e66d18cb30801b3f5e5e64866
Snigdham Roy <roysnigdham@gmail.com>
2025-05-25 21:06:45 +0530

    Changed

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
+const placesRoutes = require('../routes/places-routes');
+const usersRoutes = require('../routes/users-routes');
+const HttpError = require('../models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
+// Serve static files (uploads/images)
+app.use('/uploads/images', express.static(path.join(__dirname, '..', 'uploads', 'images')));
 
+// CORS settings
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -26,14 +28,17 @@ app.use((req, res, next) => {
   next();
 });
 
+// API Routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
+// Handle invalid routes
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
+// Error handler
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, err => {
@@ -47,16 +52,14 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
+// MongoDB connection (outside the handler)
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enstlhj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log(err));
 
+// Export Express app as serverless function
 module.exports = app;
-module.exports.handler = serverless(app); // This is required by Vercel
+module.exports.handler = serverless(app);
