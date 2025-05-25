50dd8f8e7322818f88073d9f6eff21f43781b2d1
Snigdham Roy <roysnigdham@gmail.com>
2025-05-25 22:02:46 +0530

    Changed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const serverless = require('serverless-http');
-require('dotenv').config();
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -12,11 +11,10 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
-app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
-// CORS headers
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -24,20 +22,18 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   next();
 });
 
-// Routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
-// 404 route
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
-// Error handling middleware
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, err => {
@@ -51,23 +47,14 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-// MongoDB connection (non-blocking for Vercel deployment)
-mongoose
+ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enstlhj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
-    console.error('MongoDB connection error:', err);
+    console.log(err);
   });
-
-// Default test route
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Vercel backend is working!' });
-});
-
-// Export for Vercel
-module.exports = app;
-module.exports.handler = serverless(app);
+  
