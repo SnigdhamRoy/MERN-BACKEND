1c6cc33160c3bb5e8d6c4da8423d9b0dc51dc27c
Snigdham Roy <roysnigdham@gmail.com>
2025-05-26 22:48:59 +0530

    Changed

--- models/http-error.js
+++ models/http-error.js
@@ -1,8 +1,15 @@
-class HttpError extends Error{
-    constructor(message, errorCode){
-        super(message);
-        this.code = errorCode
+class HttpError extends Error {
+  constructor(message, errorCode) {
+    super(message); // Add a message to the built-in Error
+    this.code = errorCode; // Custom property for HTTP status code
+
+    // Maintains proper stack trace (only in V8 engines like Node.js)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
     }
+
+    this.name = this.constructor.name; // Optional: Name the error class
+  }
 }
 
-module.exports = HttpError; 
+module.exports = HttpError;
--- models/user.js
+++ models/user.js
@@ -3,16 +3,38 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
-
 const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
-    places:[{type: mongoose.Types.ObjectId, required:true, ref:'Place' }]
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true, // Ensures uniqueness in DB level
+    match: [/\S+@\S+\.\S+/, 'Email format is invalid']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long']
+  },
+  image: {
+    type: String,
+    required: [true, 'Image is required']
+  },
+  places: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Place'
+    }
+  ]
 });
 
-userSchema.plugin(uniqueValidator);
+// Adds better error messages for unique fields
+userSchema.plugin(uniqueValidator, {
+  message: 'Error, expected {PATH} to be unique.'
+});
 
 module.exports = mongoose.model('User', userSchema);
-
