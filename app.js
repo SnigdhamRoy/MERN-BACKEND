ef34eae1e802924d7b4ee33e8511595c967f8fb3
Snigdham Roy <roysnigdham@gmail.com>
2025-05-27 23:00:15 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -10,13 +11,10 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
-// Parse JSON bodies
 app.use(bodyParser.json());
 
-// Serve image files
-app.use('/uploads/images', express.static(path.join(__dirname, 'uploads', 'images')));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-// Enable CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -24,43 +22,38 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   next();
 });
 
-// Define routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
-// Handle 404
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
-// Global error handler
 app.use((error, req, res, next) => {
   if (req.file) {
     fs.unlink(req.file.path, err => {
-      console.error('File cleanup error:', err);
+      console.log(err);
     });
   }
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500).json({ message: error.message || 'An unknown error occurred!' });
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-// Connect to MongoDB (without starting the server)
-mongoose
+ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.enstlhj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    console.log('✅ Connected to MongoDB');
+    app.listen(5000);
   })
   .catch(err => {
-    console.error('❌ MongoDB connection failed:', err);
+    console.log(err);
   });
-
-// Export for use in Vercel's api/index.js
-module.exports = app;
--- models/http-error.js
+++ models/http-error.js
@@ -1,15 +1,8 @@
-class HttpError extends Error {
-  constructor(message, errorCode) {
-    super(message); // Add a message to the built-in Error
-    this.code = errorCode; // Custom property for HTTP status code
-
-    // Maintains proper stack trace (only in V8 engines like Node.js)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor);
+class HttpError extends Error{
+    constructor(message, errorCode){
+        super(message);
+        this.code = errorCode
     }
-
-    this.name = this.constructor.name; // Optional: Name the error class
-  }
 }
 
-module.exports = HttpError;
+module.exports = HttpError; 
